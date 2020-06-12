4bb40b12f09419ee80a0032bdedfe4d9948b650f
linaaslt <linaaslt@gmail.com>
2020-06-12 09:17:23 +0300

    naujasProjektas

---
+++ skaiciaiTekste/src/main/java/mokymai/skaiciaiTekste/SkaiciaiTeksteApplication.java
@@ -0,0 +1,13 @@
+package mokymai.skaiciaiTekste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkaiciaiTeksteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkaiciaiTeksteApplication.class, args);
+	}
+
+}
---
+++ skaiciaiTekste/src/test/java/mokymai/skaiciaiTekste/SkaiciaiTeksteApplicationTests.java
@@ -0,0 +1,13 @@
+package mokymai.skaiciaiTekste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkaiciaiTeksteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
